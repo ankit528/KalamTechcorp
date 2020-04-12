29e2ed03372400a41f76b4d181c50cededded783
Ankit Agrawal <ankit.agrawal.emo@gmail.com>
2020-04-12 19:12:19 +0530

    Commit button click to redirect Url

--- app/src/main/java/kalamtechcorp/thinkific/com/MainActivity.java
+++ app/src/main/java/kalamtechcorp/thinkific/com/MainActivity.java
@@ -2,7 +2,10 @@ package kalamtechcorp.thinkific.com;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void register(View view) {
+        openUrl("https://kalamtechcorp.thinkific.com/cart/add_product/659426?price_id=699367");
+    }
+
+    public void demo(View view) {
+        openUrl("https://kalamtechcorp.thinkific.com/enroll/659426?et=free_trial");
+    }
+
+    public void openUrl (String url){
+        Uri uri = Uri.parse(url);
+        Intent launchWeb = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(launchWeb);
     }
 }
